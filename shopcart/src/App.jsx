16fec508605621315bcaf7cbf7de2336057d31b6
thejas-1999 <thejask074@gmail.com>
2024-01-24 14:39:40 +0530

    create a file NavBar.jsx for navbars and import to app.jsx

--- shopcart/src/App.jsx
+++ shopcart/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar />
         <Routes>
           <Route path="/" />
           <Route path="/cart" />
---
+++ shopcart/src/Components/NavBar.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <div className="links"></div>
+    </div>
+  );
+};
+export default NavBar;
