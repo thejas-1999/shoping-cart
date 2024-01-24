4f4f91543f1fbd1a595f11ef642c267c560b2fd6
thejas-1999 <thejask074@gmail.com>
2024-01-24 14:30:05 +0530

    Create the router structure for project

--- shopcart/src/App.jsx
+++ shopcart/src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" />
+          <Route path="/cart" />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
