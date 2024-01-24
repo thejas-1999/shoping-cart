fb434b34bcbb62f5dd29ff01121345f9fa8710f5
thejas-1999 <thejask074@gmail.com>
2024-01-24 14:44:09 +0530

    import phosphor for shopingcart icon and import links from react-router-dom

--- shopcart/src/Components/NavBar.jsx
+++ shopcart/src/Components/NavBar.jsx
@@ -1,8 +1,15 @@
 import { Link } from "react-router-dom";
+import { ShoppingCart } from "phosphor-react";
+
 const NavBar = () => {
   return (
     <div className="navbar">
-      <div className="links"></div>
+      <div className="links">
+        <Link to="/">Shop</Link>
+        <Link to="/cart">
+          <ShoppingCart size={32} />
+        </Link>
+      </div>
     </div>
   );
 };
